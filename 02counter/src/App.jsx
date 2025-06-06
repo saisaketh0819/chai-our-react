90c55b2b41facc14f5adeaef8db7d6843881da18
saisaketh0819 <saisaketh0819@gmail.com>
2025-06-06 14:41:28 +0530

    Before Hooks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let counter =5
+  const addValue = () =>{
+    counter = counter+1
+  }
+  return (
+    <>
+      <h1>Hello</h1>
+      <h2>Counter vlaue: {counter}</h2>
+      <button onClick={addValue}>Add</button>
+      <br/>
+      <button>Remove</button>
+    </>
+  )
+}
+
+export default App
