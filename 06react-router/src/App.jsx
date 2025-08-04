a4f054b34205d2b469b5ebea0c503f27f29097d3
saisaketh0819 <saisaketh0819@gmail.com>
2025-08-04 23:28:44 +0530

    Yws

--- 06react-router/src/App.jsx
+++ 06react-router/src/App.jsx
@@ -2,15 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-blue-800 text-white p-4 text-center'>
-        Vite + React + Tailwind CSS
-      </h1>
     </>
   )
 }
